ca52a3750a21a7153e8322ddac726b2ec9593ba3
Maylen <maylendeulofeu@gmail.com>
2023-07-02 22:33:09 -0400

    Add job_element objects and clean html

--- fake-web-scraper.py
+++ fake-web-scraper.py
@@ -6,5 +6,13 @@ page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 results = soup.find(id="ResultsContainer")
+job_elements = results.find_all("div", class_="card-content")
 
-print(results.prettify())
+for job_element in job_elements:
+    title_element = job_element.find("h2", class_="title")
+    company_element = job_element.find("h3", class_="company")
+    location_element = job_element.find("p", class_="location")
+    print(title_element)
+    print(company_element)
+    print(location_element)
+    print()
